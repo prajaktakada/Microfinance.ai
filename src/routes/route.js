83c7c2e0a21e56dc1897f344bcc5b5d823c43746
PrajaktaKada <prajaktakadam0106@.com>
2022-01-19 17:05:26 +0530

    Blogging site mini Project

---
+++ src/routes/route.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const router = express.Router();
+
+const AuthorController=require("../controllers/authorcontroller")
+const BlogsController=require("../controllers/blogscontroller")
+const Middleware=require("../middleware/Authentication")
+
+
+
+router.post('/createAuthor',AuthorController.createAuthor)
+router.post('/createBlogs',Middleware.Auth,BlogsController.createBlogs)
+router.get('/getBlogs',Middleware.Auth,BlogsController.getBlogs)
+router.put('/update/:blogId',Middleware.Auth,BlogsController.update)
+router.delete('/DeleteBlogs/:deleteId',Middleware.Auth,BlogsController.DeleteBlogs)
+router.delete('/DeleteBlogsbyQuery',Middleware.Auth,BlogsController.DeleteBlogsbyQuery)
+router.post('/login',AuthorController.login)
+
+module.exports = router;
+
+
+
+module.exports = router;
